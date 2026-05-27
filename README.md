e9511d275da1fda45a01ab66b554de2dc39308be
wjkim <henrikwjkim@gmail.com>
2026-05-27 17:02:06 +0900

    Initial release v0.1.0

---
+++ README.md
@@ -0,0 +1,34 @@
+# TimedREPL.jl
+
+Prints elapsed time in the Julia REPL after any evaluation that takes longer than a threshold.
+
+```
+julia> sleep(11)
+
+[11.001 sec]
+```
+
+The timer line overwrites the trailing newline so output stays compact.
+
+## Usage
+
+Call `enable_timed_repl` inside `atreplinit` in your `startup.jl`:
+
+```julia
+using TimedREPL
+
+atreplinit() do repl
+    if !isdefined(repl, :interface)
+        repl.interface = REPL.setup_interface(repl)
+    end
+    enable_timed_repl(repl)          # default threshold: 10 seconds
+    # enable_timed_repl(repl, 0.5)   # show timing for anything over 0.5 s
+end
+```
+
+## Installation
+
+```julia
+using Pkg
+Pkg.add("TimedREPL")
+```
---
+++ src/TimedREPL.jl
@@ -0,0 +1,26 @@
+module TimedREPL
+
+export enable_timed_repl
+
+# Wraps julia_mode.on_done to print elapsed time after each evaluation exceeding thr_sec.
+function enable_timed_repl(repl, thr_sec=10)
+    julia_mode = repl.interface.modes[1]
+
+    old_on_done = julia_mode.on_done
+    julia_mode.on_done = function (s, buf, ok)
+        t0 = time()
+        try
+            return old_on_done(s, buf, ok)
+        finally
+            elapsed_sec = time() - t0
+            if elapsed_sec > thr_sec
+                print("\033[1A")
+                print("\r")
+                print("\033[2K")
+                print("\033[90m[$(round(elapsed_sec; digits=4)) sec]\033[0m\n")
+            end
+        end
+    end
+end
+
+end # module TimedREPL
